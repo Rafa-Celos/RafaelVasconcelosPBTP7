e3e17156876aa5a80acd387d1ca5228b8405b372
Rafael Amorim <108773094+Rafa-Celos@users.noreply.github.com>
2024-03-19 23:17:24 +0000

    Initial commit

---
+++ src/Pages/BooksListPage.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import "../Components/BookList.css";
+
+export default function BooksListPage() {
+	const url = "https://t3t4-dfe-pb-grl-m1-default-rtdb.firebaseio.com/";
+	const resource = "/books.json";
+	const uri = url + resource;
+
+	const [books, setBooks] = useState([]);
+	const [filteredBooks, setFilteredBooks] = useState([]);
+	const [filterTerm, setFilterTerm] = useState("");
+
+	useEffect(() => {
+		fetch(uri)
+			.then((resp) => resp.json())
+			.then((data) => {
+				const convertedBooks = converter(data);
+				setBooks(convertedBooks);
+				setFilteredBooks(convertedBooks);
+			});
+	}, [uri]);
+
+	useEffect(() => {
+		const filtered = books.filter((book) => {
+			const titleMatch = book.title
+				.toLowerCase()
+				.includes(filterTerm.toLowerCase());
+			const authorMatch = book.author
+				.toLowerCase()
+				.includes(filterTerm.toLowerCase());
+			const genreMatch = book.genre
+				.toLowerCase()
+				.includes(filterTerm.toLowerCase());
+			return titleMatch || authorMatch || genreMatch;
+		});
+		setFilteredBooks(filtered);
+	}, [books, filterTerm]);
+
+	function converter(data) {
+		const ids = Object.keys(data);
+		const objs = Object.values(data);
+
+		return objs.map((obj, i) => {
+			return { id: ids[i], ...obj };
+		});
+	}
+
+	return (
+		<>
+			<div>
+				<input
+					type="text"
+					placeholder="Pesquisar por título, autor ou categoria"
+					size="30"
+					value={filterTerm}
+					onChange={(e) => setFilterTerm(e.target.value)}
+				/>
+			</div>
+
+			<div>
+				<ul className="books-container">
+					{filteredBooks.map((book) => (
+						<li key={book.id} className="book-item">
+							<div>Título: {book.title}</div>
+							<div>Autor: {book.author}</div>
+							<div>Categoria: {book.genre}</div>
+							<div>
+								Capa:{" "}
+								<img src={book.cover} alt={book.title} className="book-cover" />
+							</div>
+							<div>Rating: {book.rating}</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
+}
